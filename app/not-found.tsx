72ab78614fbbc9379365085ebf0c9f004550bbc9
Prince Eddie <145067724+princedigital01@users.noreply.github.com>
2025-04-15 17:09:54 -0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const notfound = () => {
   return (
-    <div justify="center" align="center" className="text-xl flex flex-col gap-4 justify-center items-center pt-20">
+    <div className="text-xl flex flex-col gap-4 justify-center items-center pt-20">
       <Image src="/images/logo.svg" width={200} height={200} alt="404" />
       <h2 className="text-3xl bold text-c-primary">Not Found</h2>
       <p>Could not find requested resource</p>
