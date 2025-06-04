93140b327eb0aa07ccb65e419366237c6b42e2e1
Prince Eddie <igberaeseedward2005@gmail.com>
2025-06-04 16:47:51 -0700

    Resolved merge conflicts and cleaned files

--- middleware.js
+++ middleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
@@ -13,19 +12,3 @@ export default async function middleware(req) {
 }
 
 export const config = { matcher: ["/dashboard"] }; // Protect dashboard
-=======
-import { auth } from "@/lib/auth";
-import { NextResponse } from "next/server";
-
-export default async function middleware(req) {
-  const session = await auth();
-
-  if (!session) {
-    return NextResponse.redirect(new URL("/login", req.url)); // Redirect to login
-  }
-
-  return NextResponse.next(); // Allow access if logged in
-}
-
-export const config = { matcher: ["/dashboard"] }; // Protect dashboard
->>>>>>> 72ab78614fbbc9379365085ebf0c9f004550bbc9
